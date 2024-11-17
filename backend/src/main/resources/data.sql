8b89a538b68f802af0e2bed8cb2b0705eaf3af52
miguelalcalah <miguelanreal@gmail.com>
2024-11-17 13:54:38 +0100

    fix: fix configurations

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,11 @@
-INSERT INTO customer (customer_id, name, surname) VALUES (1, 'Pepe', 'Hidalgo');
-INSERT INTO account (account_id, balance) VALUES (1, 1000);
-INSERT INTO transaction (transaction_id, amount, timestamp, account_id) VALUES (1, 1000, '2024-11-17 10:00:00', 1);
+-- Insert customers
+INSERT INTO customer (id, name, surname) VALUES (1, 'Pepe', 'Hidalgo');
+INSERT INTO customer (id, name, surname) VALUES (2, 'Maria', 'Garcia');
+
+-- Insert accounts
+INSERT INTO account (account_id, balance, customer_id) VALUES (1, 1000, 1);
+INSERT INTO account (account_id, balance, customer_id) VALUES (2, 1500, 2);
+
+-- Insert transactions
+INSERT INTO "transaction" (transaction_id, amount, timestamp, account_id) VALUES (1, 1000, '2024-11-17 10:00:00', 1);
+INSERT INTO "transaction" (transaction_id, amount, timestamp, account_id) VALUES (2, 1500, '2024-11-16 10:00:00', 2);
--- backend/src/test/java/com/blueharvest/bank/blueharvest/BlueharvestApplicationTests.java
+++ backend/src/test/java/com/blueharvest/bank/blueharvest/BlueharvestApplicationTests.java
@@ -5,9 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlueharvestApplicationTests {
-
 	@Test
 	void contextLoads() {
+		// Este es un simple test para verificar si el contexto se carga correctamente
 	}
-
 }
